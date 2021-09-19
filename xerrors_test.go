c8fbcf3e118adb9497d1dd158101fc290785b1cd
Yevhen Radionov <eugene.radionov@gmail.com>
2021-09-19 16:30:07 +0300

    Add more tests to xerrors

--- xerrors_test.go
+++ xerrors_test.go
@@ -1,3 +1,4 @@
+// nolint:dupl,funlen
 package xerrors
 
 import (
@@ -5,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestNewXErrs(t *testing.T) { // nolint:dupl
+func TestNewXErrs(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
@@ -142,6 +143,254 @@ func TestXErrs_Add(t *testing.T) {
 
 			if !reflect.DeepEqual(tt.want, tt.xerrs) {
 				t.Errorf("Add() want: %v, got: %v", tt.want, tt.xerrs)
+			}
+		})
+	}
+}
+
+func TestXErrs_Error(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		xerrs *XErrs
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "nil XErrs",
+			args: args{
+				xerrs: nil,
+			},
+			want: "",
+		},
+		{
+			name: "no errors",
+			args: args{
+				xerrs: &XErrs{},
+			},
+			want: "",
+		},
+		{
+			name: "1 error",
+			args: args{
+				xerrs: &XErrs{Errs: []XError{NewXErr("test msg", "test descr", nil, nil)}},
+			},
+			want: "test msg: test descr; map[]",
+		},
+		{
+			name: "multiple errors",
+			args: args{
+				xerrs: &XErrs{Errs: []XError{
+					NewXErr("test msg", "test descr", nil, nil),
+					NewXErr("test msg 2", "test descr 2", map[string]interface{}{"filed": "user_id"}, nil),
+					NewXErr("test msg 3", "test descr 3",
+						map[string]interface{}{"filed": "user_id"}, map[string]interface{}{"error_info": "some error"}),
+				}},
+			},
+			// nolint:lll
+			want: "test msg: test descr; map[];test msg 2: test descr 2; map[filed:user_id];test msg 3: test descr 3; map[filed:user_id]",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := tt.args.xerrs
+			if got := err.Error(); got != tt.want {
+				t.Errorf("Error() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestXErrs_GetErrors(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		xerrs *XErrs
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []XError
+	}{
+		{
+			name: "nil XErrs",
+			args: args{
+				xerrs: nil,
+			},
+			want: nil,
+		},
+		{
+			name: "no errors",
+			args: args{
+				xerrs: &XErrs{},
+			},
+			want: nil,
+		},
+		{
+			name: "1 error",
+			args: args{
+				xerrs: &XErrs{Errs: []XError{NewXErr("test msg", "test descr", nil, nil)}},
+			},
+			want: []XError{NewXErr("test msg", "test descr", nil, nil)},
+		},
+		{
+			name: "multiple errors",
+			args: args{
+				xerrs: &XErrs{Errs: []XError{
+					NewXErr("test msg", "test descr", nil, nil),
+					NewXErr("test msg 2", "test descr 2", map[string]interface{}{"filed": "user_id"}, nil),
+					NewXErr("test msg 3", "test descr 3",
+						map[string]interface{}{"filed": "user_id"}, map[string]interface{}{"error_info": "some error"}),
+				}},
+			},
+			want: []XError{
+				NewXErr("test msg", "test descr", nil, nil),
+				NewXErr("test msg 2", "test descr 2", map[string]interface{}{"filed": "user_id"}, nil),
+				NewXErr("test msg 3", "test descr 3",
+					map[string]interface{}{"filed": "user_id"}, map[string]interface{}{"error_info": "some error"}),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := tt.args.xerrs.GetErrors()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetErrors() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestXErrs_Len(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		xerrs *XErrs
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "nil XErrs",
+			args: args{
+				xerrs: nil,
+			},
+			want: 0,
+		},
+		{
+			name: "no errors",
+			args: args{
+				xerrs: &XErrs{},
+			},
+			want: 0,
+		},
+		{
+			name: "1 error",
+			args: args{
+				xerrs: &XErrs{Errs: []XError{NewXErr("test msg", "test descr", nil, nil)}},
+			},
+			want: 1,
+		},
+		{
+			name: "multiple errors",
+			args: args{
+				xerrs: &XErrs{Errs: []XError{
+					NewXErr("test msg", "test descr", nil, nil),
+					NewXErr("test msg 2", "test descr 2", map[string]interface{}{"filed": "user_id"}, nil),
+					NewXErr("test msg 3", "test descr 3",
+						map[string]interface{}{"filed": "user_id"}, map[string]interface{}{"error_info": "some error"}),
+				}},
+			},
+			want: 3,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := tt.args.xerrs.Len()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Len() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestXErrs_Sanitize(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		xerrs *XErrs
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "nil XErrs",
+			args: args{
+				xerrs: nil,
+			},
+		},
+		{
+			name: "no errors",
+			args: args{
+				xerrs: &XErrs{},
+			},
+		},
+		{
+			name: "1 error",
+			args: args{
+				xerrs: &XErrs{Errs: []XError{NewXErr("test msg", "test descr", nil, nil)}},
+			},
+		},
+		{
+			name: "multiple errors",
+			args: args{
+				xerrs: &XErrs{Errs: []XError{
+					NewXErr("test msg", "test descr", nil, nil),
+					NewXErr("test msg 2", "test descr 2", map[string]interface{}{"filed": "user_id"}, nil),
+					NewXErr("test msg 3", "test descr 3",
+						map[string]interface{}{"filed": "user_id"}, map[string]interface{}{"error_info": "some error"}),
+				}},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.args.xerrs.Sanitize()
+
+			for i, err := range tt.args.xerrs.GetErrors() {
+				if err.GetDescription() != "" {
+					t.Errorf("Sanitize(): xerrs[%d].Description = %s, want = \"\"", i, err.GetDescription())
+				}
 			}
 		})
 	}
